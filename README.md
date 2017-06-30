7672607a103a65be6e8c090b8e73af061b85752b
Alvin Wan <hi@alvinwan.com>
2017-06-30 02:04:04 -0700

    verified language build works

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ivytree
-test
+# IvyTree Website
+
+# Installation
+
+First, we want to clone the repository locally. Open your "Terminal" app on the Mac, and type in the following.
+
+```
+git clon
+```
--- convert.py
+++ convert.py
@@ -3,31 +3,35 @@
 import configparser
 import jinja2
 import os.path
-
-# Which language to run for
-LANGUAGE = 'en'
-assert LANGUAGE in ('en', 'ch')
+import argparse
 
 # Directory to output all files in
-OUT_DIRECTORY = os.path.join('dist', LANGUAGE)
-
-try:
-    os.mkdir(OUT_DIRECTORY)
-except FileExistsError:
-    pass
+OUT_DIRECTORY = 'dist'
 
 # Directory containing all templates
 TEMPLATE_DIRECTORY = './templates'
 
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('language', metavar='L', type=str,
+                    help='Which configuration file did you update? "en" or "ch"')
+args = parser.parse_args()
+
+try:
+    os.mkdir(OUT_DIRECTORY)
+    os.mkdir(os.path.join(OUT_DIRECTORY, args.language))
+except FileExistsError:
+    pass
+
 config = configparser.ConfigParser()
-config.read(LANGUAGE + '.config')
+config.read(args.language + '.config')
 env = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_DIRECTORY))
 
+assert args.language in ('en', 'ch'), 'Language must be one of ("en", "ch")'
 
 for filename, context in config.items():
     if not filename.endswith('html'):
         continue
     source_path = os.path.join(TEMPLATE_DIRECTORY, filename)
-    out_path = os.path.join(OUT_DIRECTORY, filename)
+    out_path = os.path.join(OUT_DIRECTORY, args.language, filename)
     with open(out_path, 'w') as f:
         f.write(env.get_template(filename).render(context))
